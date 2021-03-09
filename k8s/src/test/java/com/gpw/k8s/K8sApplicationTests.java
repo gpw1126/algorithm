d8746c809c07bb6bc1637f67ab4b2c3323a1680c
gpw1126 <123837732@qq.com>
2021-03-09 13:03:53 +0800

    基于Kubernetes和Docker的容器云平台功能测试代码推送

---
+++ k8s/src/test/java/com/gpw/k8s/K8sApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gpw.k8s;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class K8sApplicationTests {
+
+    public K8sApplicationTests() {
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
